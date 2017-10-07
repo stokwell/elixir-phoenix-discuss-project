a0f180b62fc704ad67c0a683f2990dda1b9fec9c
Vitaly Lyapunov <stokwell1@gmail.com>
2017-10-07 22:40:12 +0200

    transforming requests with plugs

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -11,13 +11,19 @@ defmodule Discuss.AuthController do
     signin(conn, changeset)
   end
 
+  def signout(conn, _params) do
+    conn
+    |> configure_session(drop: true)
+    |> redirect(to: topic_path(conn, :index))
+  end
+
   defp signin(conn, changeset) do
     case insert_or_update_user(changeset) do
       {:ok, user} ->
         conn
         |> put_flash(:info, "Welcome")
         |> put_session(:user_id, user.id)
-        |> redirect(to: topic_path(conn, :index))    
+        |> redirect(to: topic_path(conn, :index))
 
       {:error, _error} ->
         conn
