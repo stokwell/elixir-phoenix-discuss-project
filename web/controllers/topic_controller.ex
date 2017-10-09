6b7a0c499144d514cf59613bedc5d7ad7c6bd635
Vitaly Lyapunov <stokwell1@gmail.com>
2017-10-10 00:59:11 +0200

    comments model

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -4,13 +4,18 @@ defmodule Discuss.TopicController do
   alias Discuss.Topic
 
   plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
-  plug :check_topic_owner when action im [:update, :edit, :delete]
+  plug :check_topic_owner when action in [:update, :edit, :delete]
 
   def index(conn, params) do
     IO.inspect(conn.assigns)
     topics = Repo.all(Topic)
 
     render conn, "index.html", topics: topics
+  end
+
+  def show(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    render conn, "show.html", topic: topic
   end
 
 
@@ -37,7 +42,7 @@ defmodule Discuss.TopicController do
   end
 
 
-  def edit(conn, %{"id"=> topic_id}) do
+  def edit(conn, %{"id" => topic_id}) do
     topic = Repo.get(Topic, topic_id)
     changeset = Topic.changeset(topic)
 
@@ -72,14 +77,14 @@ defmodule Discuss.TopicController do
 
   def check_topic_owner(conn, _params) do
     %{params: %{"id" => topic_id}} = conn
-    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do 
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
       conn
     else
       conn
       |> put_flash(:error, "You cannot edit that")
       |> redirect(to: topic_path(conn, :index))
       |> halt()
-
+    end
   end
 
 end
